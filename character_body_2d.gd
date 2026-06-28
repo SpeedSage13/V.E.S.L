456bf17c9b0042545860350935b7c2cd0e9f90d6
Cessamon12 <successchambers10@gmail.com>
2026-06-28 11:26:42 -0400

    V.E.S.L - Placeholder Animation Fix
    - Fixed Animation Bug w/ Running and Turning
    - Made Gravity an Integer 
    - Added Gradual speed gain for sprint speed
    
    [needed fixes]
    - Smoothing out camera movement
    - Smoothing out sprint/walk speed
    - Figure out air controls/momentum
    - Fix bug with Jump animation

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,31 +1,41 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const RUN_SPEED = 700.0
+const SPEED = 10
+const RUN_SPEED = 600.0
 const JUMP_VELOCITY = -400.0
+var gravity = 25
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += 20.0
+		velocity.y += gravity
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("Input_Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	#Handles Idle Animation supposedly??
-	if not Input.is_action_pressed("Input_Left") or not Input.is_action_pressed("Input_Right") or not Input.is_action_pressed("Input_Up") or not Input.is_action_pressed("Input_Down") and is_on_floor():
+		
+	#Handles Idle, Run, and Jump Animations.
+	if not Input.is_action_pressed("Input_Left") and not Input.is_action_pressed("Input_Right") and not Input.is_action_pressed("Input_Up") and not Input.is_action_pressed("Input_Down") and is_on_floor():
 		$Character_Sprites.play("Idle")
-	
-	if Input.is_action_pressed("Input_Left"):
-		$Character_Sprites.flip_h = true
-		$Character_Sprites.play("Run")
-	elif Input.is_action_pressed("Input_Right"):
+	else:
+		if Input.is_action_pressed("Input_Left") and is_on_floor():
+			$Character_Sprites.flip_h = true
+			$Character_Sprites.play("Run")
+			if not is_on_floor():
+				$Character_Sprites.stop()
+		elif Input.is_action_pressed("Input_Right") and is_on_floor():
 			$Character_Sprites.flip_h = false
 			$Character_Sprites.play("Run")
+			if not is_on_floor():
+				$Character_Sprites.stop()
+		elif Input.is_action_pressed("Input_Up") and is_on_floor():
+			$Character_Sprites.play("Jump")
+			
 
-	#Set running speed to a different button, may change later.
+	#Set a Sprint Action for the player to speed up with.
+	#should smoothly switch between sprinting and walking.
 	var running := Input.is_action_pressed("Sprint")
 	var direction := Input.get_axis("Input_Left", "Input_Right")
 	if direction and not running:
@@ -33,6 +43,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if direction and running:
-		velocity.x = direction * RUN_SPEED
-
+		while not velocity.x == RUN_SPEED:
+			velocity.x += 1
 	move_and_slide()
