73b9ff10e80d1fb61d26c666940814bbf6c776fc
2pctrobot <2pctrobot@gmail.com>
2026-06-30 11:34:15 +0900

    slime turnaround

--- slime_p.gd
+++ slime_p.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 const SPEED = 60
 
+var direction = 1
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +13,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += 60 * delta
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+	position.x += direction * 60 * delta
