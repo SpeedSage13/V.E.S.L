b3c1d5daea4596e005483e28d8f6e396f6cf4955
2pctrobot <2pctrobot@gmail.com>
2026-07-01 14:08:07 +0900

    green slime moves doesn't hit yet

---
+++ green_slime.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 30.0
+const JUMP_VELOCITY = -400.0
+var direction = -1
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	velocity.x = SPEED * direction
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
+
+
+
+func _on_wallcheck_body_entered(body: Node2D) -> void:
+	flip()
+
+
+
+
+
+func flip():
+	direction *= -1
+	scale.x *= -1
